75fcea38ba10cfdbd0a70d1f354a9e10c8e02885
jefailhamprayoga13 <jefailhamprayoga13@gmail.com>
2023-03-26 15:03:42 +0700

    .

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -2,7 +2,7 @@
     <h2 class="text-2xl font-bold text-center text-slate-800">Daftar Barang</h2>
     <div class="p-5 rounded-lg bg-orange-100 shadow-lg m-2 flex" v-for="(item, index) in items" :key="index">
         <div class="text-lg ">
-            <img :src="item.img">
+            <img src="../assets/img/headshet.png">
             <h5>{{ item.name }}</h5>
             <p class="font-bold">Rp.{{ item.harga }}</p>
             <p>{{ item.deskripsi }}</p>
